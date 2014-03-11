ad1aaecb4354b1e8336bf25af35bae16eb6534e2
emillamm <e_millerkiller@hotmail.com>
2014-03-11 19:52:48 +0100

    init

---
+++ server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var express = require('express');
+
+//User validation
+var auth = express.basicAuth(function(user, pass) {     
+   return (user == "super" && pass == "ring");
+},'DARKTOSPARK preview');
+
+/**
+ * Main application file
+ */
+
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+// Application Config
+var config = require('./lib/config/config');
+
+var app = express();
+
+// Express settings
+require('./lib/config/express')(app);
+
+// Routing
+require('./lib/routes')(app, auth);
+
+// Start server
+app.listen(config.port, function () {
+  console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
+});
+
+// Expose app
+exports = module.exports = app;
